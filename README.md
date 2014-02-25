915050d587e11649d6b8d62268b035705f268173
Ella Baron <ebaron@apple.com>
2014-02-25 14:42:56 -0800

    got symbol mapping script to work

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 shadowtrace
 ===========
+
+Instructions
+============
+
+1. Copy trace/trace.c into your directory tree and build your excecutable with -finstrument-functions flag and trace.c. Do not forget -g option to preserve symbols. Exclude as many shared
+libraries as possible. Link them statically. If you are interested in the library calls, also build that library with -finstrument-functions and -g.
+
+2. In the running directory do the following:
+
+touch TRACE
+
+3. Run yout executable. When it is done, the output will be written into TRACE file.
+
+4. Build trace package:
+
+make
+
+5. Generate call tree:
+
+src/post-process <input file> <output file>
+
+Input file here is TRACE file.
+
+6. Address to symbol mapping:
+
+script/get-symbols.pl -e <executable file> -i <optpus of pre-process step> -o <symbol mapping file>
---
+++ scripts/get-symbols.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use integer;
+
+my %options = (); 
+
+sub usage
+{
+    print "-e <executable file> -i <tree file> -o <symbols file>\n";
+    exit;
+}
+
+sub init
+{
+    use Getopt::Std;
+    getopts("i:o:e:", \%options) or usage();
+    usage() if not ($options{o} and $options{i} and $options{e});
+}
+
+my %symbols = ();
+
+sub load_symbols
+{
+    my ($exe_file) = @_;
+    print "Loading symbols from '$exe_file'...\n";
+    if (-x $exe_file)
+    {
+        open FILE, "nm -C $exe_file |" or die "Failed to open pipe on \'nm -C $exe_file\'\n";
+        while (<FILE>)
+        {
+            my $line = $_;
+            chomp $line;
+            if ($line =~ /([0-9a-fA-F]+)\s+\w\s+(.*)$/)
+            {
+                my $addr = $1;
+                my $symbol = $2;
+                $addr =~ s/^0+//;
+                $symbols{$addr} = $symbol;
+            }
+        }
+    }
+}
+
+my %func_addr = ();
+
+sub load_input
+{
+    my ($input_file) = @_; 
+    print "Loading '$input_file'...\n";
+    open(INPUTFILE, "<$input_file") or die "Failed to open input file: '$input_file'\n";
+    while (<INPUTFILE>)
+    {
+        my $line = $_;
+        chomp $line;
+        my @fields = split (/ /, $line);
+        my $addr = $fields[1];
+        if (exists $func_addr{$addr})
+        {
+            $func_addr{$addr}++;
+        }
+        else
+        {
+            $func_addr{$addr} = 1;
+        }
+    }
+    return;
+}
+
+sub dump_output
+{
+    my ($output_file) = @_; 
+    print "Writing to  '$output_file'...\n";
+    open(OUTPUTFILE, ">$output_file") or die "Failed to open output file: '$output_file'\n";
+    foreach my $addr (sort keys %func_addr)
+    {
+        my $stripped_addr = $addr;
+        $stripped_addr =~ s/^0x//;
+        my $symbol = (exists $symbols{$stripped_addr})? $symbols{$stripped_addr} : $stripped_addr;
+        print OUTPUTFILE "$stripped_addr $func_addr{$addr} $symbol\n";
+    }
+    close(OUTPUTFILE);
+}
+
+init();
+
+load_symbols($options{e});
+load_input($options{i});
+dump_output($options{o});
